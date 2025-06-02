f6f5736dc467ae8031b8da8cd68b27fdf945f911
EinePerson <einenperson@gmail.com>
2025-06-02 21:19:10 +0200

    updated key initalizer

--- src/main/java/de/igelstudios/igelengine/common/startup/KeyInitializer.java
+++ src/main/java/de/igelstudios/igelengine/common/startup/KeyInitializer.java
@@ -1,9 +1,6 @@
 package de.igelstudios.igelengine.common.startup;
 
-import de.igelstudios.igelengine.client.keys.HIDInput;
-import de.igelstudios.igelengine.client.keys.KeyListener;
-import de.igelstudios.igelengine.client.keys.MouseDragListener;
-import de.igelstudios.igelengine.client.keys.MouseMoveListener;
+import de.igelstudios.igelengine.client.keys.*;
 
 import java.util.*;
 
@@ -15,6 +12,7 @@ public class KeyInitializer {
     private List<MouseMoveListener> moveListeners = new ArrayList<>();
 
     private List<MouseDragListener> dragListeners = new ArrayList<>();
+    private List<MouseClickListener> clickListeners = new ArrayList<>();
 
     public void add(int key, String name) {
         this.keys.put(Integer.valueOf(key), name);
@@ -31,6 +29,7 @@ public class KeyInitializer {
     public void add(MouseDragListener listener) {
         this.dragListeners.add(listener);
     }
+    public void add(MouseClickListener listener) {this.clickListeners.add(listener);}
 
     public void register(HIDInput input) {
         Objects.requireNonNull(input);
@@ -41,5 +40,7 @@ public class KeyInitializer {
         this.moveListeners.forEach(input::registerMoveListener);
         Objects.requireNonNull(input);
         this.dragListeners.forEach(input::registerDragListener);
+        Objects.requireNonNull(input);
+        this.clickListeners.forEach(input::registerMouseClickListener);
     }
 }
