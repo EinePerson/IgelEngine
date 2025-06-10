dc5747b985114ce9b9ab631b048163642ace4e0b
EinePerson <einenperson@gmail.com>
2025-06-10 11:59:42 +0200

    added Utility methods to Line

--- src/main/java/de/igelstudios/igelengine/client/graphics/Line.java
+++ src/main/java/de/igelstudios/igelengine/client/graphics/Line.java
@@ -14,31 +14,85 @@ public class Line implements BatchContent {
     private float r,g,b,a;
     private boolean dirty;
     private boolean remove;
+    private boolean mirror;
 
-    public Line(Vector2f start, Vector2f end,float thickness) {
-        this(start,(end.x - start.x) / (end.y - start.y),(float) Math.sqrt(Math.pow(end.x - start.x,2) + Math.pow(end.y - start.y,2)),thickness);
+    private Line(Vector2f start){
+        this.start = start;
+        dirty = true;
     }
 
-    public Line(Vector2f start,float angleD,float length,float thickness) {
+    public Line(Vector2f start, Vector2f end,float thickness) {
+        this(start,(end.x - start.x) / (end.y - start.y),(float) Math.sqrt(Math.pow(end.x - start.x,2) + Math.pow(end.y - start.y,2)),thickness,
+                false,0,0,0,1);
+    }
+
+    public Line(Vector2f start,float angleD,float length,float thickness,boolean mirror){
+        this(start,angleD,length,thickness,mirror,0,0,0,1);
+    }
+
+    public Line(Vector2f start,float angleD,float length,float thickness,boolean mirror,float r,float g,float b,float a) {
         this.start = start;
         this.angle = (float) Math.toRadians(angleD);
         this.length = length;
         this.thickness = thickness;
 
-        //double angle = Math.tanh(incline);
-        double deltaX = length * Math.cos(angle);
-        double deltaY = length * Math.sin(angle);
-        end = new Vector2f((float) (start.x + deltaX), (float) (start.y + deltaY));
-        Vector2f directional = new Vector2f((float) -deltaY, (float) deltaX).normalize().mul(thickness);
-        startUp = directional.add(start);
-        Vector2f distance = new Vector2f(end).sub(start);
-        endUp = new Vector2f(startUp).add(distance);
+        this.mirror = mirror;
 
-        r = 0.0f;
-        g = 0.0f;
-        b = 0.0f;
-        a = 1.0f;
+        this.r = r;
+        this.g = g;
+        this.b = b;
+        this.a = a;
+
+        recalculate();
+
         dirty = true;
+    }
+
+    public Line cloneFromEnd(float angleD,float length,float thickness,boolean mirror){
+        Line clone = new Line(new Vector2f(end));
+        clone.endUp = new Vector2f(endUp);
+        clone.end = new Vector2f(end);
+        clone.startUp = new Vector2f(startUp);
+        boolean rec = false;
+        if(this.angle != Math.toRadians(angleD)) rec = true;
+        clone.angle = (float) Math.toRadians(angleD);
+
+        if(this.length != length) rec = true;
+        clone.length = length;
+
+        if(this.thickness != thickness)rec = true;
+        clone.thickness = thickness;
+
+        if(this.mirror != mirror) rec = true;
+        clone.mirror = mirror;
+
+        clone.r = r;
+        clone.g = g;
+        clone.b = b;
+        clone.a = a;
+        if(rec){
+            clone.recalculate();
+        }
+
+        return clone;
+    }
+
+    public Line cloneMoved(Vector2f deltaPox){
+        return cloneMoved(deltaPox.x,deltaPox.y);
+    }
+
+    public Line cloneMoved(float deltaX, float deltaY){
+        Line clone = clone();
+        clone.start.x += deltaX;
+        clone.start.y += deltaY;
+        clone.end.x += deltaX;
+        clone.end.y += deltaY;
+        clone.startUp.x += deltaX;
+        clone.startUp.y += deltaY;
+        clone.endUp.x += deltaX;
+        clone.endUp.y += deltaY;
+
+        return clone;
     }
 
     public void remove(){
@@ -65,7 +119,29 @@ public class Line implements BatchContent {
         return r;
     }
 
+    private void recalculate(){
+        double deltaX = length * Math.cos(angle);
+        double deltaY = length * Math.sin(angle);
+        end = new Vector2f((float) (start.x + deltaX), (float) (start.y + deltaY));
+
+        Vector2f directional = (mirror ?new Vector2f((float) deltaY, (float) -deltaX):new Vector2f((float) -deltaY, (float) deltaX)).normalize().mul(thickness);
+        startUp = directional.add(start);
+        endUp = new Vector2f(startUp).add(end).sub(start);
+
+        dirty = true;
+    }
+
+    public Line mirror(boolean mirror) {
+        if(this.mirror == mirror)return this;
+        this.mirror = mirror;
+
+        recalculate();
+
+        return this;
+    }
+
     public Line setThickness(float thickness) {
+        if(thickness == this.thickness)return this;
         this.thickness = thickness;
 
         endUp.sub(end).mul(thickness).add(end);
@@ -76,22 +152,16 @@ public class Line implements BatchContent {
     }
 
     public Line setIncline(float angleD) {
+        if((float) Math.toRadians(angleD) == this.angle)return this;
         this.angle = (float) Math.toRadians(angleD);
 
-        double deltaX = length * Math.cos(angle);
-        double deltaY = length * Math.sin(angle);
-        end = new Vector2f((float) (start.x + deltaX), (float) (start.y + deltaY));
-        Vector2f directional = new Vector2f((float) -deltaY, (float) deltaX).mul(thickness);
-        startUp = directional.add(start);
-        Vector2f distance = new Vector2f(end).sub(start);
-        endUp = new Vector2f(startUp).add(distance);
+        recalculate();
 
-        dirty = true;
         return this;
     }
 
     public Line setLength(float length) {
-
+        if(this.length == length)return this;
 
         endUp.sub(startUp).div(this.length).mul(length).add(startUp);
         end.sub(endUp).div(this.length).mul(length).add(endUp);
@@ -150,5 +220,25 @@ public class Line implements BatchContent {
 
     public Vector2f getStartUp() {
         return startUp;
+    }
+
+    @Override
+    public Line clone(){
+        Line clone = new Line(new Vector2f(start));
+        clone.end = new  Vector2f(end);
+        clone.startUp = new  Vector2f(startUp);
+        clone.endUp = new  Vector2f(endUp);
+
+        clone.length = this.length;
+        clone.thickness = this.thickness;
+        clone.mirror = this.mirror;
+        clone.angle = this.angle;
+
+        clone.r = this.r;
+        clone.g = this.g;
+        clone.b = this.b;
+        clone.a = this.a;
+
+        return clone;
     }
 }
